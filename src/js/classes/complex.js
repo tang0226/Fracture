8c7898200f7ffd09a7e6ab0ac2843579d5b196e7
tang0226 <58008827+tang0226@users.noreply.github.com>
2024-07-04 15:34:18 -0700

    Hardcode complex arrays instead of calling Complex()

--- src/js/classes/complex.js
+++ src/js/classes/complex.js
@@ -2,8 +2,6 @@
 COMPLEX NUMBERS
 ******************************/ 
 
-// No prototype, too tedious and unnecessary
-// to type "new Complex()" each time
 function Complex(re, im) {
   return [re, im];
 };
@@ -31,41 +29,40 @@ Complex.isEqual = function(c1, c2) {
 };
 
 Complex.add = function(c1, c2) {
-  return Complex(c1[0] + c2[0], c1[1] + c2[1]);
+  return [c1[0] + c2[0], c1[1] + c2[1]];
 };
 
 Complex.sub = function(c1, c2) {
-  return Complex(c1[0] - c2[0], c1[1] - c2[1]);
+  return [c1[0] - c2[0], c1[1] - c2[1]];
 };
 
 Complex.mul = function(c1, c2) {
-  return Complex(
+  return [
     c1[0] * c2[0] - c1[1] * c2[1],
     c1[0] * c2[1] + c1[1] * c2[0]
-  );
+  ];
 };
 
 Complex.div = function(c1, c2) {
-  return Complex(
+  return [
     (c1[0] * c2[0] + c1[1] * c2[1]) / (c2[0] * c2[0] + c2[1] * c2[1]),
     (c1[1] * c2[0] - c1[0] * c2[1]) / (c2[0] * c2[0] + c2[1] * c2[1])
-  );
+  ];
 };
 
 Complex.sq = function(c) {
-  return Complex(
+  return Complex[
     c[0] ** 2 - c[1] ** 2,
     2 * c[0] * c[1]
-  );
+  ];
 };
 
 // Only positive integer exponents, for now
 Complex.exp = function(c, e) {
-  let r = c;
-  let currE = 1;
-  while (currE < e) {
+  let r = [c[0], c[1]];
+  let currE = 0;
+  while (++currE < e) {
     r = Complex.mul(r, c);
-    currE++;
   }
   return r;
 };
@@ -78,5 +75,5 @@ Complex.abs = function(c) {
 
 // The complex conjugate a+bi is a-bi
 Complex.conj = function(c) {
-  return Complex(c[0], -c[1]);
+  return [c[0], -c[1]];
 }
