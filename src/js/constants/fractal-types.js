5a334176857e0cac75df4749f1dd8ce5edbe5ddf
tang0226 <58008827+tang0226@users.noreply.github.com>
2024-07-05 13:51:13 -0700

    Add rest of fractals

--- src/js/constants/fractal-types.js
+++ src/js/constants/fractal-types.js
@@ -87,4 +87,103 @@ const FRACTAL_TYPES = {
       ];
     },
   },
+
+  multiship: {
+    id: "multiship",
+    meta: {
+      iterationType: "mandelbrot",
+      reqExponent: true,
+      juliaEquivalent: "multishipJulia",
+    },
+    iterFunc: function(z, c, params) {
+      return Complex.add(
+        Complex.exp(
+          [Math.abs(z[0]), Math.abs(z[1])],
+          params.e
+        ), c
+      );
+    }
+  },
+
+  multishipJulia: {
+    id: "multishipJulia",
+    meta: {
+      iterationType: "julia",
+      reqJuliaConst: true,
+      reqExponent: true,
+      mandelEquivalent: "multiship",
+    },
+    iterFunc: function(z, params) {
+      return Complex.add(
+        Complex.exp(
+          [Math.abs(z[0]), Math.abs(z[1])],
+          params.e
+        ), params.c
+      );
+    }
+  },
+
+  tricorn: {
+    id: "tricorn",
+    meta: {
+      iterationType: "mandelbrot",
+      juliaEquivalent: "tricornJulia",
+    },
+    iterFunc: function(z, c, _params) {
+      return [
+        z[0] * z[0] - z[1] * z[1] + c[0],
+        -2 * z[0] * z[1] + c[1],
+      ];
+    },
+  },
+
+  tricornJulia: {
+    id: "tricornJulia",
+    meta: {
+      iterationType: "julia",
+      reqJuliaConst: true,
+      mandelEquivalent: "tricorn",
+    },
+    iterFunc: function(z, params) {
+      return [
+        z[0] * z[0] - z[1] * z[1] + params.c[0],
+        -2 * z[0] * z[1] + params.c[1],
+      ];
+    },
+  },
+
+  multicorn: {
+    id: "multicorn",
+    meta: {
+      iterationType: "mandelbrot",
+      reqExponent: true,
+      juliaEquivalent: "multicornJulia",
+    },
+    iterFunc: function(z, c, params) {
+      return Complex.add(
+        Complex.exp(
+          [z[0], -z[1]],
+          params.e
+        ), c
+      );
+    }
+  },
+
+  multicornJulia: {
+    id: "multicornJulia",
+    meta: {
+      iterationType: "julia",
+      reqJuliaConst: true,
+      reqExponent: true,
+      mandelEquivalent: "multicorn",
+    },
+    iterFunc: function(z, params) {
+      return Complex.add(
+        Complex.exp(
+          [z[0], -z[1]],
+          params.e
+        ), params.c
+      );
+    }
+  },
 };
